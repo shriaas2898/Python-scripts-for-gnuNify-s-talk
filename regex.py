32a1982763e8523395e6ccf7de781e8334f3a4bc
shriaas2898 <aasthashrivastava410@gmail.com>
2019-02-17 23:26:44 +0530

    Added documentation

--- regex.py
+++ regex.py
@@ -1,4 +1,5 @@
-import re
-phNumRegex = re.compile(r"\d\d-\d{10}")
-mo = phNumRegex.search("My Number is 91-8888888888.")
-print("Phone Number found:"+mo.group())
+#Python code to search mobile number in supplied string through regex
+import re #importing regex module
+phNumRegex = re.compile(r"\d\d-\d{10}") #creating regex for phone number
+mo = phNumRegex.search("My Number is 91-8888888888.") #searching regex in string
+print("Phone Number found:"+mo.group())#printing all the matching results
